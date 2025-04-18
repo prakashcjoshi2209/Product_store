832ce9360f905e4c3c89b6dc252589f14d7585f7
prakashjoshi <prakashchandrajoshi2209@gmail.com>
2025-04-18 15:20:55 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	
+		build: {
+		  outDir: 'public'  // Ensure the build output goes to the 'public' directory
+		},
+	  
 	plugins: [react()],
 	server: {
 		proxy: {
